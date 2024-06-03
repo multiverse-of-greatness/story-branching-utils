25844f9cc472f48e0283da1a1baacdf634f2caa2
Chollakorn Nimpattanavong <chollakorn.nimp@gmail.com>
2024-06-03 21:44:56 +0900

    chore: add word list from previous work

--- src/config.py
+++ src/config.py
@@ -2,5 +2,6 @@ from pathlib import Path
 
 DATA_PATH = Path("data")
 RESULT_PATH = Path("results")
+WORDS_PATH = Path("words")
 
 CRITERION = ["coherence", "inspiration", "narrative fluency", "readability", "word complexity"]
