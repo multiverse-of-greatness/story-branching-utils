db9416d0bdb51fb79a49d567be210db68016e4df
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2024-06-11 16:20:16 +0900

    feat: bias checking

--- compile_result.py
+++ compile_result.py
@@ -1,6 +1,7 @@
 import typer
 from dotenv import load_dotenv
 
+from src.compile_result.bias import core_bias_evaluation
 from src.compile_result.evaluation import core_objective_evaluation
 from src.compile_result.word_cloud import core_word_cloud_aggregation
 
@@ -15,6 +16,11 @@ def summarize_obj_eval():
 @app.command()
 def generate_word_cloud():
     core_word_cloud_aggregation()
+
+
+@app.command()
+def check_bias():
+    core_bias_evaluation()
 
 
 if __name__ == "__main__":
---
+++ src/compile_result/bias.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+
+from loguru import logger
+from rich.progress import track
+
+from src.config import OUTPUTS_PATH
+
+
+def core_bias_evaluation():
+    positive_word_list_path = Path("words") / "positive-words.txt"
+    positive_word_list = positive_word_list_path.read_text().splitlines()
+    negative_word_list_path = Path("words") / "negative-words.txt"
+    negative_word_list = negative_word_list_path.read_text().splitlines()
+
+    Path("outputs").mkdir(exist_ok=True)
+
+    results = {
+        "baseline": {
+            "positive": 0,
+            "negative": 0
+        },
+        "proposed": {
+            "positive": 0,
+            "negative": 0
+        }
+    }
+
+    narrative_texts = [None, None]
+    path_to_stories = [OUTPUTS_PATH / 'all-text-baseline.txt', OUTPUTS_PATH / 'all-text-proposed.txt']
+    for idx, path in enumerate(path_to_stories):
+        narrative_texts[idx] = path.read_text().split(" ")
+
+    for idx, narrative_text in enumerate(narrative_texts):
+        logger.info(f"Processing {path_to_stories[idx]}")
+        positive_count = 0
+        negative_count = 0
+
+        for word in track(narrative_text):
+            if word in positive_word_list:
+                positive_count += 1
+            elif word in negative_word_list:
+                negative_count += 1
+
+        results["baseline" if idx == 0 else "proposed"]["positive"] = positive_count
+        results["baseline" if idx == 0 else "proposed"]["negative"] = negative_count
+
+        logger.info(f"Positive words count: {positive_count}")
+        logger.info(f"Negative words count: {negative_count}")
+
+    bias_result_path = OUTPUTS_PATH / "bias_result.csv"
+    with bias_result_path.open("w") as f:
+        f.write("model,positive,negative\n")
+        f.write(f"baseline,{results['baseline']['positive']},{results['baseline']['negative']}\n")
+        f.write(f"proposed,{results['proposed']['positive']},{results['proposed']['negative']}\n")
+    logger.info(f"Results saved to {bias_result_path}")
